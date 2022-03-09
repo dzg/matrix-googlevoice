c9838c826ba81bfe3f754e2efd982a0ba138ded2
Ze'ev <466557+dzg@users.noreply.github.com>
2022-03-09 09:43:26 -0800

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -3,7 +3,7 @@ module.exports = {
    matrixDomain: 'yourdomain.com',
    matrixBotId: '@GVbot:yourdomain.com', 
    matrixBotAccessToken: "syt_abcdefghijklmnopqrsutwxyz123456789",
-   matrixYourIds: ['@you:yourdomain.com'], // array of users to invite to new rooms
+   matrixYourIds: ['@you:yourdomain.com','@you:beeper.com'], // array of users to invite to new rooms
    gmailId: 'you@gmail.com',
    gmailPw: 'abcdefghijklmnop', // Use an App Password, see https://support.google.com/accounts/answer/185833
    roomAvatarURL: 'mxc://yourdomain.com/ABCDEFGHIJKLMNOPQRSTUVWX' // Avatar for all new GV bridged rooms
